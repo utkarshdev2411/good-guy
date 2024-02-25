047d5c893fcdea2180767326d58625d524e05f69
utkbella <114491739+utkbella@users.noreply.github.com>
2024-02-26 03:30:09 +0530

    Created Contact left portion.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import Button from './Components/Button/button';
+import Contact from './Components/Contact/contact';
 import Introduction from './Components/Introduction/introduction';
 import Line from './Components/Line/line';
 import Navbar from './Components/Navbar/navbar';
+
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
      <Line/>
      <Introduction/>
      <Button/>
+     <Line/>
+     <Contact/>
     </div>
   );
 }
---
+++ src/Components/Contact/contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import  './contact.css'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+       <div className='c-left'><h1> Contact Us </h1>
+       <h3>Let’s get this conversation started. Tell us a bit about yourself,  and we will get in touch as soon as we can. </h3>
+       
+       </div>
+       <div><h1>Hello</h1></div>
+    </div>
+  )
+}
+
+export default Contact
