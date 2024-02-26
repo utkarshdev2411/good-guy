7d0a503b0cf034ccd03024490dc48d1db986eeb9
utkbella <114491739+utkbella@users.noreply.github.com>
2024-02-26 23:49:58 +0530

    Completed right ContactUs

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -1,16 +1,43 @@
 import React from 'react'
-import  './contact.css'
+import './contact.css'
 
 const Contact = () => {
-  return (
-    <div className='contact'>
-       <div className='c-left'><h1> Contact Us </h1>
-       <h3>Let’s get this conversation started. Tell us a bit about yourself,  and we will get in touch as soon as we can. </h3>
+    return (
+        <div className='contact '>
+            <div className='c-left'><h1> Contact Us </h1>
+                <h3>Let’s get this conversation started. Tell us a bit about yourself,  and we will get in touch as soon as we can. </h3>
+
+            </div>
+            <div className='c-right'>
+
+            <form>
+                
+                <div className='name'>
+                <div className='fname'>
+                    <label>First Name:</label>
+                    <input type='text'></input>
+                </div>
+                <div className='fname'>
+                    <label>Last Name:</label>
+                    <input type='text'></input>
+                </div>
+                </div>
+                
+                <div className='fmail'>
+                    <label>Work Email</label>
+                    <input></input>
+                    <label>Message:</label>
+                    <textarea></textarea>
+                </div>
+              
+            </form>
+
+            </div>
        
-       </div>
-       <div><h1>Hello</h1></div>
-    </div>
-  )
+
+      
+        </div>
+    )
 }
 
 export default Contact
