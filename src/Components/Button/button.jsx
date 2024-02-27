10d69296954b13ccedf5c4ed01ed382fd54a5135
utkbella <114491739+utkbella@users.noreply.github.com>
2024-02-28 00:56:30 +0530

    Updated intro button

--- src/Components/Button/button.jsx
+++ src/Components/Button/button.jsx
@@ -5,7 +5,7 @@ import Arrow from '../../image/arrow.png';
 const Button = () => {
   return (
     <div className='button'>
-      <button><span className='text'>Try Now</span><img className='arrow' src={Arrow}></img></button>
+      <button><span className='try'> Try Now</span><img className='arrow' src={Arrow}></img></button>
     </div>
   )
 }
