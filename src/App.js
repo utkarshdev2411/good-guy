7edbde2e41e06b68c6aa433c8b08efac1b844dd4
utkbella <114491739+utkbella@users.noreply.github.com>
2024-03-01 11:11:07 +0530

    Created crd component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Button from './Components/Button/button';
+import Card from './Components/Card/card';
 import Contact from './Components/Contact/contact';
 import Footer from './Components/Footer/footer';
 import Introduction from './Components/Introduction/introduction';
 import Line from './Components/Line/line';
 import Navbar from './Components/Navbar/navbar';
+import Services from './Components/Services/services';
 
 
 function App() {
@@ -15,7 +17,9 @@ function App() {
      <Introduction/>
      <Button/>
      <Line/>
+     <Card/>
      <Contact/>
+     <Services/>
         
      <Footer/>
     </div>
