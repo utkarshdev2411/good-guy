49ef2d3f557744da4a8f73ef8bb817c05a2f708c
Utkarsh Sharma <114491739+utkarshdev2411@users.noreply.github.com>
2024-03-28 22:43:18 +0530

    Update introduction.jsx

--- src/Components/Introduction/introduction.jsx
+++ src/Components/Introduction/introduction.jsx
@@ -4,7 +4,7 @@ import './introduction.css'
 const Introduction = () => {
     return (
         <div className='introduction' >
-            <h1 className='heading'>    "Shaping Your Digital Destiny: Development to Marketig, We've Got Your Back!"</h1>
+            <h1 className='heading'>    "Shaping Your Digital Destiny: Development to Marketing, We've Got Your Back!"</h1>
             <h3 className='content'><strong>Good Guy</strong> gurantees your online success! From concept to creation, we craft exceptional digital experiences, paving the way for your digital dominance. Trust us to shape your digital destiny!</h3>
         </div>
     )
